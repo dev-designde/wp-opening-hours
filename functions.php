639e2f2458902455ee2ea110328a52088ed4530c
DEV-DESIGN <125412944+dev-designde@users.noreply.github.com>
2023-10-18 16:03:15 +0200

    Update functions.php
    Changes:
    Switched the language of the days and messages to English.
    Added dual time slots for Monday and Friday.
    Code now iterates through all time ranges for the current day.
    Messages like "Closing soon" and "Reopening at" have been added to inform the user about upcoming changes in operational status.
    
    
    Before:
    Days and messages were in German.
    Only one time slot was possible per day.
    The code did not account for dual time ranges.
    No additional messages like "Closing soon" or "Reopening at."
    
    
    After:
    Days and messages are in English.
    Monday and Friday can have two different opening time slots.
    The code iterates through all time ranges for the current day and updates the operational status accordingly.
    Additional messages like "Closing soon" and "Reopening at" have been added.

--- functions.php
+++ functions.php
@@ -5,60 +5,58 @@ function render_opening_hours() {
     $current_time = date('H:i');
 
     $opening_hours = array(
-        'Monday'    => array('08:00-12:00', '15:00-18:00'),
-        'Tuesday'   => array('08:00-12:00', '15:00-18:00'),
-        'Wednesday' => array('08:00-12:00'),
-        'Thursday'  => array('08:00-12:00', '15:00-18:00'),
-        'Friday'    => array('08:00-12:00'),
-        'Saturday'  => 'Closed',
+        'Monday'    => array('08:00-13:00', '15:00-18:00'),
+        'Tuesday'   => array('09:00-19:00'),
+        'Wednesday' => array('09:00-19:00'),
+        'Thursday'  => array('09:00-19:00'),
+        'Friday'    => array('08:00-13:00', '15:00-18:00'),
+        'Saturday'  => array('11:00-16:00'),
         'Sunday'    => 'Closed'
     );
 
     $output = '<table>';
     foreach ($opening_hours as $day => $hours) {
         $output .= '<tr>';
-        switch ($day) {
-            case $current_day:
-                if ($hours != 'Closed') {
-                    $is_open = false;
-                    $closing_soon = false;
-                    $reopening_later = false;
-                    $next_opening_time = '';
-                    foreach ($hours as $range) {
-                        list($start, $end) = explode('-', $range);
-                        if ($current_time >= $start && $current_time <= $end) {
-                            $is_open = true;
-                            $closing_time = date('H:i', strtotime($end) - 3600);  // 1 hour before closing
-                            if ($current_time >= $closing_time) {
-                                $closing_soon = true;
-                            }
-                            break;
-                        } elseif ($current_time < $start) {
-                            if (!$reopening_later) {
-                                $next_opening_time = $start;
-                                $reopening_later = true;
-                            }
+        if ($day === $current_day) {
+            if ($hours != 'Closed') {
+                $is_open = false;
+                $closing_soon = false;
+                $reopening_later = false;
+                $next_opening_time = '';
+                foreach ($hours as $range) {
+                    list($start, $end) = explode('-', $range);
+                    if ($current_time >= $start && $current_time <= $end) {
+                        $is_open = true;
+                        $closing_time = date('H:i', strtotime($end) - 3600);  // 1 hour before closing
+                        if ($current_time >= $closing_time) {
+                            $closing_soon = true;
+                        }
+                        break;
+                    } elseif ($current_time < $start) {
+                        if (!$reopening_later) {
+                            $next_opening_time = $start;
+                            $reopening_later = true;
                         }
                     }
-                    if ($is_open && $closing_soon) {
-                        $output .= "<td>{$day}:</td><td style='color: darkorange;'>Closing soon</td>";
-                    } elseif ($reopening_later) {
-                        $output .= "<td>{$day}:</td><td>Reopening at {$next_opening_time}</td>";
-                    } elseif ($is_open) {
-                        $output .= "<td>{$day}:</td><td style='color: darkgreen;'>We are open!</td>";
-                    } else {
-                        $output .= "<td>{$day}:</td><td>" . implode(' and ', $hours) . "</td>";
-                    }
-                } else {
-                    $output .= "<td>{$day}:</td><td style='color: darkred;'>Closed</td>";
                 }
-                break;
-            case 'Saturday':
-            case 'Sunday':
+                if ($is_open && $closing_soon) {
+                    $output .= "<td>{$day}:</td><td style='color: darkorange;'>Closing soon</td>";
+                } elseif ($reopening_later) {
+                    $output .= "<td>{$day}:</td><td>Reopening at {$next_opening_time}</td>";
+                } elseif ($is_open) {
+                    $output .= "<td>{$day}:</td><td style='color: darkgreen;'>Open</td>";
+                } else {
+                    $output .= "<td>{$day}:</td><td>" . implode(' and ', $hours) . "</td>";
+                }
+            } else {
                 $output .= "<td>{$day}:</td><td style='color: darkred;'>Closed</td>";
-                break;
-            default:
+            }
+        } else {
+            if ($hours != 'Closed') {
                 $output .= "<td>{$day}:</td><td>" . implode(' and ', $hours) . "</td>";
+            } else {
+                $output .= "<td>{$day}:</td><td style='color: darkred;'>Closed</td>";
+            }
         }
         $output .= '</tr>';
     }
@@ -66,4 +64,4 @@ function render_opening_hours() {
 
     return $output;
 }
-add_shortcode('sprechzeiten-dev-design', 'render_opening_hours');
+add_shortcode('opening_hours', 'render_opening_hours');
